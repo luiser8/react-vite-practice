2a5e1cd74a292e44a245e1940f93e99800ac9391
Luis E Rondon <leduardo.rondon@gmail.com>
2022-07-06 17:59:50 -0400

    Agregar actualizacion de arrays

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,41 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [posts, setPosts] = useState([]); //Creas tu estado de tipo Array
+  //Actualización de Arrays
+  const [array, setArray] = useState(
+    [
+      {
+        'id': 1,
+        'name': 'Array 1',
+        'active': true
+      },
+      {
+        'id': 2,
+        'name': 'Array 2',
+        'active': false
+      },
+      {
+        'id': 3,
+        'name': 'Array 3',
+        'active': false
+      }
+    ]
+  );
+
+    //Método para agregar arrays
+    const agregarArray = async () => {
+      const newArray = 
+      {
+        'id': 4,
+        'name': 'Array 4',
+        'active': true
+      }
+      setArray((array) => [...array, newArray]);
+
+      const filtrado = array.filter((ar => ar.active === true));
+      console.log(filtrado);
+
+    }
 
   //Método para buscar al API los posts
   const getPosts = async () => {
@@ -18,6 +53,16 @@ function App() {
   //Renderizar los posts
   return (
     <div className="App">
+      {/* para agregar los arrays */}
+      <button onClick={async () => await agregarArray()}>Agregar array</button>
+      {/* lista de todos los arrays */}
+      <div>
+        {Object.keys(array).map((item) => (
+          <p key={array[item].id}>{array[item].name}</p>
+        ))}
+      </div>      
+      {/* Otra sección */}
+      {/* para renderizar los posts */}
       <ul>
         {Object.keys(posts).map((post) => (
           <li key={posts[post].id}>{posts[post].id} - {posts[post].title}</li>
